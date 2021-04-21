5f3a632faa70e28a39750c9583ae02ad1b59fbc6
“lucifer2355” <dgajjar999@gmail.com>
2021-04-21 12:44:19 +0530

    add all header icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,6 +16,11 @@ const Header = () => {
     <header>
       <div>
         <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
       </div>
       <Image src='https://links.papareact.com/ua6' width={200} height={100} />
     </header>
