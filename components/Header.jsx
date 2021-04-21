f36ea7e334dff0fee5393c40ec3cf55da9aced10
“lucifer2355” <dgajjar999@gmail.com>
2021-04-21 12:29:44 +0530

    create header item component & add heroicon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Image from "next/image";
 
+import HeaderItem from "./HeaderItem";
+
 const Header = () => {
   return (
     <header>
       <h1>This is Header</h1>
+
+      <div>
+        <HeaderItem title='HOME' />
+      </div>
       <Image src='https://links.papareact.com/ua6' width={200} height={100} />
     </header>
   );
---
+++ components/HeaderItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderItem = ({ title, icon }) => {
+  return <div></div>;
+};
+
+export default HeaderItem;
