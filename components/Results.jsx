0c78531a6a020fc359a336e4339401371d61bea2
“lucifer2355” <dgajjar999@gmail.com>
2021-04-22 15:15:37 +0530

    dispplay the movie thumnail

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Thumbnail from "./Thumbnail";
+
 const Results = ({ results }) => {
-  return <div></div>;
+  return (
+    <div>
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 };
 
 export default Results;
---
+++ components/Thumbnail.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+const Thumbnail = ({ result }) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+  return (
+    <div>
+      <Image
+        layout='responsive'
+        height={1080}
+        width={1920}
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
+        }
+      />
+    </div>
+  );
+};
+
+export default Thumbnail;
