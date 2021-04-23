4e2e026aeab854002256dd38312930c45605b984
“lucifer2355” <dgajjar999@gmail.com>
2021-04-23 22:37:37 +0530

    add the image border radius

--- components/MoviePoster.jsx
+++ components/MoviePoster.jsx
@@ -5,8 +5,13 @@ const MoviePoster = ({ posterImageURL }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div className='flex justify-center mt-10'>
-      <Image width={300} height={450} src={`${BASE_URL}${posterImageURL}`} />
+    <div className='flex justify-center mt-10 overflow-hidden'>
+      <Image
+        className='rounded-md'
+        width={300}
+        height={450}
+        src={`${BASE_URL}${posterImageURL}`}
+      />
     </div>
   );
 };
