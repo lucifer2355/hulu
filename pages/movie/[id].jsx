4646e3138e7d3259e35719af8dc888c162311847
“lucifer2355” <dgajjar999@gmail.com>
2021-04-23 23:00:20 +0530

    add movie title & tagline

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -8,6 +8,11 @@ const Movie = ({ results }) => {
   return (
     <div>
       <MoviePoster posterImageURL={results.poster_path} />
+
+      <div className='pl-10 mt-10'>
+        <p className='text-2xl text-white font-serif'>{results.title}</p>
+        <p className='text-sm text-gray-300 font-normal'>{results.tagline}</p>
+      </div>
     </div>
   );
 };
