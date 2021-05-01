0c9f9693174b603f74ca888a8c2c500d35cf1e34
“lucifer2355” <dgajjar999@gmail.com>
2021-05-01 12:34:45 +0530

    display the person's enters in movie list

--- pages/person/[id].jsx
+++ pages/person/[id].jsx
@@ -2,38 +2,42 @@ import React from "react";
 import { ArrowLeftIcon } from "@heroicons/react/outline";
 
 import MoviePoster from "../../components/MoviePoster";
+import Results from "../../components/Results";
 
-const Person = ({ results }) => {
-  console.log(results);
+const Person = ({ results, getMovieCredits }) => {
+  console.log(getMovieCredits);
 
   return (
-    <div className='pl-10'>
-      <MoviePoster posterImageURL={results.profile_path} />
-      <h2 className='text-2xl font-serif mt-10  text-white font-bold'>
-        {results.name}
-      </h2>
-      <h2 className='text-sm  mt-2 '>{results.birthday}</h2>
+    <>
+      <div className='pl-10'>
+        <MoviePoster posterImageURL={results.profile_path} />
+        <h2 className='text-2xl font-serif mt-10  text-white font-bold'>
+          {results.name}
+        </h2>
+        <h2 className='text-sm  mt-2 '>{results.birthday}</h2>
 
-      <div className='mt-6  pr-5'>
-        <h2 className='text-lg text-white font-bold'>THE BIOGRAPHY</h2>
-        <p className='text-sm mt-2 text-justify'>{results.biography}</p>
+        <div className='mt-6  pr-5'>
+          <h2 className='text-lg text-white font-bold'>THE BIOGRAPHY</h2>
+          <p className='text-sm mt-2 text-justify'>{results.biography}</p>
+        </div>
+
+        <div
+          className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5 cursor-pointer transition transform hover:-translate-y-1'
+          onClick={() => router.back()}
+        >
+          <ArrowLeftIcon className='h-3' />
+          <h2>Back</h2>
+        </div>
       </div>
-
-      <div
-        className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5 cursor-pointer transition transform hover:-translate-y-1'
-        onClick={() => router.back()}
-      >
-        <ArrowLeftIcon className='h-3' />
-        <h2>Back</h2>
-      </div>
-
       <div className='mt-6'>
         <h2 className='text-xl font-serif text-white leading-3'>
           ALSO ENTERS IN
         </h2>
         <span className='text-xs'>MOVIES</span>
+
+        <Results results={getMovieCredits.results} />
       </div>
-    </div>
+    </>
   );
 };
 
@@ -46,9 +50,14 @@ export async function getServerSideProps(context) {
     `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.API_KEY}`
   ).then((res) => res.json());
 
+  const getMovieCredits = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&sort_by=popularity.desc&with_cast=${id}`
+  ).then((res) => res.json());
+
   return {
     props: {
       results,
+      getMovieCredits,
     },
   };
 }
