cebc8d59a632b174202f825a8e1652a581cce9cc
“lucifer2355” <dgajjar999@gmail.com>
2021-05-01 16:14:15 +0530

    add responsive css

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -13,56 +13,62 @@ const Movie = ({ results, cast, recommended }) => {
   const router = useRouter();
 
   return (
-    <div>
-      <MoviePoster posterImageURL={results.poster_path} />
+    <>
+      <div className='xl:flex xl:justify-around md:flex md:justify-around'>
+        <MoviePoster posterImageURL={results.poster_path} />
 
-      <div className='pl-10  mt-10'>
-        <p className='text-2xl text-white font-serif'>{results.title}</p>
-        <p className='text-sm text-gray-300 font-normal'>{results.tagline}</p>
-      </div>
+        <div className='pl-10 flex flex-col xl:w-8/12 md:w-6/12'>
+          <div className='  mt-10'>
+            <p className='text-2xl text-white font-serif'>{results.title}</p>
+            <p className='text-sm text-gray-300 font-normal'>
+              {results.tagline}
+            </p>
+          </div>
 
-      <div className='flex mt-4 pl-10 pr-10 justify-between'>
-        <div className='flex'>
-          <Rating rating={results.vote_average / 2} />
-          <h5 className='pl-2 text-sm'>{results.vote_average}</h5>
+          <div className='flex mt-4 pr-10 justify-between'>
+            <div className='flex'>
+              <Rating rating={results.vote_average / 2} />
+              <h5 className='pl-2 text-sm'>{results.vote_average}</h5>
+            </div>
+
+            <p className='text-sm'>
+              {results.spoken_languages[0].name} / {results.runtime} MIN./{" "}
+              {results.release_date.split("-")[0]}
+            </p>
+          </div>
+
+          <div className='mt-6 '>
+            <h2 className='text-sm font-serif font-bold'>THE GENRES</h2>
+            <Genres genres={results.genres} />
+          </div>
+
+          <div className='mt-6 '>
+            <h2 className='text-sm font-serif font-bold'>THE SYNOPSIS</h2>
+            <p className='mt-1 text-sm pr-5'>{results.overview}</p>
+          </div>
+
+          <div className='mt-6 '>
+            <h2 className='text-sm font-serif font-bold'>THE CAST</h2>
+            <Cast cast={cast} />
+          </div>
+
+          <div className='flex mt-6  space-x-3'>
+            <MovieWebLinks
+              title='Website'
+              Icon={LinkIcon}
+              link={results.homepage}
+            />
+            <MovieWebLinks title='IMDB' isImage />
+          </div>
+
+          <div
+            className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5  cursor-pointer transition transform hover:-translate-y-1'
+            onClick={() => router.back()}
+          >
+            <ArrowLeftIcon className='h-3' />
+            <h2>Back</h2>
+          </div>
         </div>
-
-        <p className='text-sm'>
-          {results.spoken_languages[0].name} / {results.runtime} MIN./{" "}
-          {results.release_date.split("-")[0]}
-        </p>
-      </div>
-
-      <div className='mt-6 pl-10'>
-        <h2 className='text-sm font-serif font-bold'>THE GENRES</h2>
-        <Genres genres={results.genres} />
-      </div>
-
-      <div className='mt-6 pl-10'>
-        <h2 className='text-sm font-serif font-bold'>THE SYNOPSIS</h2>
-        <p className='mt-1 text-sm pr-5'>{results.overview}</p>
-      </div>
-
-      <div className='mt-6 pl-10'>
-        <h2 className='text-sm font-serif font-bold'>THE CAST</h2>
-        <Cast cast={cast} />
-      </div>
-
-      <div className='flex mt-6 pl-10 space-x-3'>
-        <MovieWebLinks
-          title='Website'
-          Icon={LinkIcon}
-          link={results.homepage}
-        />
-        <MovieWebLinks title='IMDB' isImage />
-      </div>
-
-      <div
-        className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5 ml-10 cursor-pointer transition transform hover:-translate-y-1'
-        onClick={() => router.back()}
-      >
-        <ArrowLeftIcon className='h-3' />
-        <h2>Back</h2>
       </div>
 
       <div>
@@ -73,7 +79,7 @@ const Movie = ({ results, cast, recommended }) => {
 
         <Results results={recommended.results} />
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/person/[id].jsx
+++ pages/person/[id].jsx
@@ -1,39 +1,43 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { ArrowLeftIcon } from "@heroicons/react/outline";
 
 import MoviePoster from "../../components/MoviePoster";
 import Results from "../../components/Results";
 
 const Person = ({ results, getMovieCredits }) => {
-  console.log(getMovieCredits);
+  const router = useRouter();
 
   return (
     <>
-      <div className='pl-10'>
+      <div className='pl-10 xl:flex xl:justify-around md:flex md:justify-around'>
         <MoviePoster posterImageURL={results.profile_path} />
-        <h2 className='text-2xl font-serif mt-10  text-white font-bold'>
-          {results.name}
-        </h2>
-        <h2 className='text-sm  mt-2 '>{results.birthday}</h2>
 
-        <div className='mt-6  pr-5'>
-          <h2 className='text-lg text-white font-bold'>THE BIOGRAPHY</h2>
-          <p className='text-sm mt-2 text-justify'>{results.biography}</p>
-        </div>
+        <div className='flex flex-col xl:w-8/12 md:w-6/12'>
+          <h2 className='text-2xl font-serif mt-10  text-white font-bold'>
+            {results.name}
+          </h2>
+          <h2 className='text-sm  mt-2 '>{results.birthday}</h2>
 
-        <div
-          className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5 cursor-pointer transition transform hover:-translate-y-1'
-          onClick={() => router.back()}
-        >
-          <ArrowLeftIcon className='h-3' />
-          <h2>Back</h2>
+          <div className='mt-6  pr-5'>
+            <h2 className='text-lg text-white font-bold'>THE BIOGRAPHY</h2>
+            <p className='text-sm mt-2 text-justify'>{results.biography}</p>
+          </div>
+
+          <div
+            className='flex items-center space-x-2 border w-24 rounded-2xl pl-5 pr-5 pt-1 pb-1 mt-5 cursor-pointer transition transform hover:-translate-y-1'
+            onClick={() => router.back()}
+          >
+            <ArrowLeftIcon className='h-3' />
+            <h2>Back</h2>
+          </div>
         </div>
       </div>
-      <div className='mt-6'>
-        <h2 className='text-xl font-serif text-white leading-3'>
+      <div className='mt-10'>
+        <h2 className='text-xl font-serif text-white leading-3 pl-10'>
           ALSO ENTERS IN
         </h2>
-        <span className='text-xs'>MOVIES</span>
+        <span className='text-xs pl-10'>MOVIES</span>
 
         <Results results={getMovieCredits.results} />
       </div>
